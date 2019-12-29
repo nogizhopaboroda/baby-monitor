46cd63dcde03b1ad3f9ffa046a1435be7db0a041
nogizhopaboroda <al.zhukoff@gmail.com>
2019-12-29 02:42:59 +0100

    add websocket player

--- client/index.js
+++ client/index.js
@@ -1,4 +1,5 @@
 import WSAvcPlayer from 'ws-avc-player';
+import PCMPlayer from './pcm-player';
 
 const HOST = process.env.HOST || window.location.hostname;
 const PORT = process.env.PORT || window.location.port;
@@ -14,7 +15,27 @@ const canvas = document.body.appendChild(wsavc.AvcPlayer.canvas);
 document.body.appendChild(canvas);
 
 
-const audioElement = new Audio(`http://${HOST}:${PORT}/audio`);
-audioElement.controls = true;
-audioElement.preload = 'none';
-document.body.appendChild(audioElement);
+
+const player = new PCMPlayer({
+  encoding: '16bitInt',
+  channels: 1,
+  sampleRate: 44100,
+  flushingTime: 0
+});
+
+const audioStreamUrl = `ws://${HOST}:${PORT}/audio-stream`;
+
+const button = document.createElement('button');
+button.innerHTML = 'play';
+document.body.appendChild(button);
+button.addEventListener('click', () => {
+  var ws = new WebSocket(audioStreamUrl);
+  ws.binaryType = 'arraybuffer';
+  ws.addEventListener('message',function(event) {
+    const data = new Uint8Array(event.data);
+    player.feed(data);
+  });
+});
+
+
+
