06d238a348fa34feff5a7156fcf7e136d0513b03
nogizhopaboroda <al.zhukoff@gmail.com>
2020-01-23 16:23:26 +0100

    better development build

--- development/build.sh
+++ development/build.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-current_path=$( dirname "${BASH_SOURCE[0]}" )
+BASE_IMAGE_TAG="baby-monitor-base:latest"
 
-docker build --tag baby-monitor-development:latest $current_path
+current_path=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+parent_directory_path=$(dirname "$current_path")
+
+. $parent_directory_path/build.sh $BASE_IMAGE_TAG
+
+docker build --build-arg BASE_IMAGE=baby-monitor-base:latest --tag baby-monitor-development:latest $current_path
