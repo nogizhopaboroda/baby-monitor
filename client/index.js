e66ad9d6a7b6575241e2166fcf9c1b16fc9787d7
nogizhopaboroda <al.zhukoff@gmail.com>
2020-02-24 01:47:04 +0100

    EXPERIMENT: split stream on ffe

--- client/index.js
+++ client/index.js
@@ -2,10 +2,33 @@ import WSAvcPlayer from './ws-avc-player';
 import PCMPlayer from './pcm-player';
 import createVisualiser from './visualiser';
 
+import muxjs from 'mux.js';
+// const muxjs = require("mux.js");
+let transmuxer = new muxjs.mp4.Transmuxer({ 'remux': false });
+var aa = new muxjs.codecs.h264.H264Stream();
+// debugger;
+// console.log(muxjs);
+window.muxjs = muxjs
+// debugger;
+
+transmuxer.on("data", function(segment) {
+console.log(456);
+debugger;
+});
+
+aa.on("data", function(segment) {
+console.log(8989);
+debugger;
+});
+
 const HOST = process.env.HOST || window.location.hostname;
 const PORT = process.env.PORT || window.location.port;
 
 const $appContainer = document.querySelector('#app-container');
+
+
+let tmpA = new Uint8Array();
+let arrs = [];
 
 class StreamClient {
   constructor(options = {}){
@@ -31,14 +54,25 @@ class StreamClient {
 
 const wsavc = new WSAvcPlayer();
 
+let i = 0;
 const videoStream = new StreamClient({
-  url: `ws://${HOST}:8001`,
+  url: `ws://${HOST}:8002`,
   onOpen(){
     wsavc.framesList = []
     console.log('Connected to video stream');
   },
   onMessage(data){
-    wsavc.feed(data);
+    //TODO: this can be optimised
+    tmpA = Int8Array.from([...tmpA, ...data])
+    const index = tmpA.findIndex(function (element, index, array) {
+      return element === 0 && array[index + 1] === 0 && array[index + 2] === 0  && array[index + 3] === 1;
+    })
+    if(~index){
+      const chunk = tmpA.slice(0, index);
+      tmpA = tmpA.slice(index + 4);
+      console.log('got chunk');
+      wsavc.feed(chunk);
+    }
   }
 });
 
@@ -51,7 +85,7 @@ const player = new PCMPlayer({
   encoding: '16bitInt',
   channels: 1,
   sampleRate: 16000,
-  flushingTime: 0
+  // flushingTime: 0
 });
 
 const audioStream = new StreamClient({
@@ -63,6 +97,8 @@ const audioStream = new StreamClient({
     player.feed(data);
   }
 });
+
+window.player = player;
 
 
 const $audioLevel = document.querySelector('#audio-level');
