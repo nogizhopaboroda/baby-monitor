84f60436f6a8ce98e0e92ace542e0e7ef94a1eec
nogizhopaboroda <al.zhukoff@gmail.com>
2020-03-07 01:20:33 +0100

    add trash

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-TAG=${1:-baby-monitor:latest}
-
-docker build -f Dockerfile.alpine --tag $TAG .
--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker run -it --privileged -v /dev/snd/:/dev/snd/ -v /opt/vc/lib:/opt/vc/lib --device /dev/vchiq:/dev/vchiq -p 5000:5000 -p 8000:8000 -p 8001:8001 baby-monitor:latest
