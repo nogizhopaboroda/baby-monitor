41207fe77ccde70f4f931b24210893fbce1ee327
nogizhopaboroda <al.zhukoff@gmail.com>
2020-04-17 03:47:04 +0200

    fix

--- streamer/src/entrypoint.sh
+++ streamer/src/entrypoint.sh
@@ -2,15 +2,13 @@
 
 echo "creating streams"
 
-streams=(
-    $VIDEO_RECORDING_PIPE
-    $RAW_VIDEO_RECORDING_PIPE
-    $AUDIO_RECORDING_PIPE
-    $RAW_AUDIO_RECORDING_PIPE
-    $RAW_AUDIO_RECORDING_PIPE_1
-)
-
-for stream in "${streams[@]}"; do
+for stream in \
+  $VIDEO_RECORDING_PIPE \
+  $RAW_VIDEO_RECORDING_PIPE \
+  $AUDIO_RECORDING_PIPE \
+  $RAW_AUDIO_RECORDING_PIPE \
+  $RAW_AUDIO_RECORDING_PIPE_1
+do
   stream_path=$PIPES_DIR/$stream
   echo "creating $stream_path"
   mkfifo $stream_path
