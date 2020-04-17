2704feaa6c15220371ef82f18d0490aaeaec4f01
nogizhopaboroda <al.zhukoff@gmail.com>
2020-04-17 03:36:56 +0200

    fix entrypoint

--- streamer/src/entrypoint.sh
+++ streamer/src/entrypoint.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 
-echo "creating stream named pipes"
-mkfifo $PIPES_DIR/{$VIDEO_RECORDING_PIPE,$RAW_VIDEO_RECORDING_PIPE,$AUDIO_RECORDING_PIPE,$RAW_AUDIO_RECORDING_PIPE,$RAW_AUDIO_RECORDING_PIPE_1}
+echo "creating streams"
+
+streams=(
+  $VIDEO_RECORDING_PIPE
+  $RAW_VIDEO_RECORDING_PIPE
+  $AUDIO_RECORDING_PIPE
+  $RAW_AUDIO_RECORDING_PIPE
+  $RAW_AUDIO_RECORDING_PIPE_1
+)
+
+for stream in "${streams[@]}"; do
+  stream_path=$PIPES_DIR/$stream
+  echo "creating $stream_path"
+  mkfifo $stream_path
+done
 
 
 echo "starting the monitor"
