c449ea9d07eb4126360f9f6ed78c2f89a2e4a4b3
nogizhopaboroda <al.zhukoff@gmail.com>
2020-04-28 01:09:43 +0200

    componentify app controller

--- client/src/index.js
+++ client/src/index.js
@@ -3,33 +3,41 @@ import './components/audio-player';
 import './components/fullscreen-button';
 import './components/temperature-humidity';
 
+class ApplicationController extends HTMLElement {
+  connectedCallback() {
 
+    const handleVisibilityChange = () => {
+      const videoPlayerElement = this.querySelector('video-player');
+      if (document.hidden) {
+        console.log('lost focus. stopping player');
+        videoPlayerElement.player.pause();
+      } else {
+        console.log('got focus. starting player');
+        videoPlayerElement.player.play();
+      }
+    };
 
-const $appContainer = document.querySelector('#app-container');
+    document.addEventListener(
+      'visibilitychange',
+      handleVisibilityChange,
+      false,
+    );
 
-const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-
-$appContainer.innerHTML = `
-  <video-player type="${isMobile ? 'yuv' : 'h264'}"></video-player>
-  <audio-player type="${isMobile ? 'aac' : 'raw'}"></audio-player>
-  <temperature-humidity id="temp-humidity"></temperature-humidity>
-  <fullscreen-button id="fullscreen-button" class="app-icon"></fullscreen-button>
-`;
-
-const videoPlayerElement = $appContainer.querySelector('video-player');
-videoPlayerElement.setAttribute('type', isMobile ? 'yuv' : 'h264');
-const videoPlayer = videoPlayerElement.player;
-
-
-const handleVisibilityChange = () => {
-  if (document.hidden) {
-    console.log('lost focus. stopping player');
-    videoPlayer.pause();
-  } else {
-    console.log('got focus. starting player');
-    videoPlayer.play();
+    this.render();
   }
-};
 
-document.addEventListener('visibilitychange', handleVisibilityChange, false);
+  render() {
+    this.innerHTML = `
+      <video-player type="${this.isMobile() ? 'yuv' : 'h264'}"></video-player>
+      <audio-player type="${this.isMobile() ? 'aac' : 'raw'}"></audio-player>
+      <temperature-humidity id="temp-humidity"></temperature-humidity>
+      <fullscreen-button id="fullscreen-button" class="app-icon"></fullscreen-button>
+    `;
+  }
 
+  isMobile(){
+    return /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+  }
+}
+
+customElements.define('application-controller', ApplicationController);
